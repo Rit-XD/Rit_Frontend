b62f217d18f19eef63d2d59f37410e51de7f8bf1
Bram-Colleman <r0820751@student.thomasmore.be>
2024-05-16 16:39:44 +0200

    add planner component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,4 @@
+import { Planner } from '@/components/planner/Planner'
 import {fetchUser} from '@/lib/user/fetchUser'
 import {redirect} from 'next/navigation'
 
@@ -5,5 +6,7 @@ export default async function Dashboard() {
   const user = await fetchUser()
   if (!user) redirect(`/login`)
 
-  return <main></main>
+  return <main>
+    <Planner/>
+  </main>
 }
---
+++ src/components/planner/Planner.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import React from "react"
+import css from './Planner.module.scss'
+import { fromModule } from "@/utils/styler/Styler"
+
+const styles = fromModule(css);
+
+export const Planner: React.FC = () => {
+
+
+
+    return (
+    <div className={styles.container()}>
+        <div className={styles.container.planner()}>
+            <h3>Plan een rit</h3>
+            <div className={styles.container.planner.inputs()}>
+                <select name="passenger" id="select-passenger" className={styles.container.planner.inputs.input()}>
+                    <option value="" selected disabled>Passagier toevoegen</option>
+                    <option value="">1</option>
+                    <option value="">2</option>
+                    <option value="">3</option>
+                </select>
+                <input type="text" name="destination" id="input-destination" placeholder="Bestemming" className={styles.container.planner.inputs.input()}/>
+                <input type="datetime-local" name="date" id="input-date" placeholder="Tijdstip" className={styles.container.planner.inputs.input()}/>
+            </div>
+        </div>
+    </div>
+    )
+}
