81a0062e3cbad3ec55ad4ede0bc69b15f2922cc4
Bram-Colleman <r0820751@student.thomasmore.be>
2024-05-19 14:10:01 +0200

    add array of passengers

--- src/components/planner/Planner.tsx
+++ src/components/planner/Planner.tsx
@@ -8,24 +8,39 @@ import { Map } from "../map/Map";
 const styles = fromModule(css);
 
 export const Planner: React.FC = () => {
+    const [passengers, setPassengers] = React.useState<string[]>([]);
 
-
+    const appendToArray = (nextElement: string) => {
+        if (nextElement !== ""){
+            const next_arr = [...passengers, nextElement];
+            setPassengers(next_arr);
+        }
+      }
+      const removeIndexFromArray = (index: number) => {
+        const next_arr = [...passengers.slice(0, index), ...passengers.slice(index + 1)];
+        setPassengers(next_arr);
+      }
 
     return (
     <div className={styles.container()}>
         <div className={styles.container.planner()}>
             <h3>Plan een rit</h3>
             <div className={styles.container.planner.inputs()}>
-                <select name="passenger" id="select-passenger" className={styles.container.planner.inputs.input()}>
+                <select name="passenger" id="select-passenger" className={styles.container.planner.inputs.input()} onChange={(e)=>appendToArray(e.target.value)}>
                     <option value="" selected disabled>Passagier toevoegen</option>
-                    <option value="">1</option>
-                    <option value="">2</option>
-                    <option value="">3</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
                 </select>
                 <input type="text" name="destination" id="input-destination" placeholder="Bestemming" className={styles.container.planner.inputs.input()}/>
                 <input type="datetime-local" name="date" id="input-date" placeholder="Tijdstip" className={styles.container.planner.inputs.input()}/>
             </div>
             <div className={styles.container.planner.inputs()}>
+                <div>
+                    {passengers.map((passenger, index) => 
+                        <p>{passenger}</p>
+                    )}
+                </div>
                 <div>
                     <div>
                     <input type="radio" name="routetype" id="routetype-both" className={styles.container.planner.inputs.radio()}/>
@@ -41,6 +56,9 @@ export const Planner: React.FC = () => {
         </div>
         <div className={styles.container.map()}>
             <Map/>
+        </div>
+        <div>
+            {passengers}
         </div>
     </div>
     )
