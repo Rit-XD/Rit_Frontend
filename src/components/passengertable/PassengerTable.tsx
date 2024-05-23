37c371b5e7b42083690a11984ff24da8e8135dca
Bram-Colleman <r0820751@student.thomasmore.be>
2024-05-23 16:50:12 +0200

    change table to nextui table

--- src/components/passengertable/PassengerTable.tsx
+++ src/components/passengertable/PassengerTable.tsx
@@ -7,6 +7,11 @@ import {fromModule} from '@/utils/styler/Styler'
 import React, {useEffect, useState} from 'react'
 import {fetchPassengers} from '../planner/FetchPlanner'
 import css from './PassengerTable.module.scss'
+import { useRouter } from 'next/navigation'
+import { Pagination, Spinner, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow, getKeyValue } from '@nextui-org/react'
+import { Loader } from '@/ui/loader/Loader'
+import { useAsyncList } from '@react-stately/data'
+import { Passenger } from '@/types/passenger.type'
 
 const styles = fromModule(css)
 
@@ -28,6 +33,7 @@ export const PassengerTable: React.FC<{
   const [selectedPassengerId, setSelectedPassengerId] = useState<string | null>(
     null
   )
+
 
   const closeEditPassenger = () => {
     setSelectedPassengerId(null)
@@ -65,14 +71,54 @@ export const PassengerTable: React.FC<{
   }, [user])
 
   const sort = (key: string) => {
-    console.log(passengers)
     setPassengers(passengers!.sort((a, b) => a.firstname.localeCompare(b.firstname)))
-    console.log(passengers)
   }
 
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [page, setPage] = React.useState(1);
+  const rowsPerPage = 2;
+  const pages = passengers? Math.ceil(passengers!.length / rowsPerPage) : 1;
+
+  const items = React.useMemo(() => {
+    const start = (page - 1) * rowsPerPage;
+    const end = start + rowsPerPage;
+
+    return passengers?.length != undefined? passengers!.slice(start, end) : passengers;
+  }, [page, passengers]);
+
+
+
+  let list = useAsyncList({
+    async load({signal}) {
+      let res = await fetch('https://swapi.py4e.com/api/people/?search', {
+        signal,
+      });
+      let json = await res.json();
+      setIsLoading(false);
+
+      return {
+        items: json.results,
+      };
+    },
+    async sort({items, sortDescriptor}) {
+      return {
+        items: passengers!.sort((a, b) => {
+          let first = String(a[sortDescriptor.column as keyof Passenger]);
+          let second = String(b[sortDescriptor.column as keyof Passenger]);
+          let cmp = first < second ? -1 : 1;
+
+          if (sortDescriptor.direction === "descending") {
+            cmp *= -1;
+          }
+
+          return cmp;
+        }),
+      };
+    },
+  });
   return (
     <div className={styles.tableContainer()}>
-      <table className={styles.table()}>
+      {/* <table className={styles.table()}>
         <thead>
           <tr>
             <th onClick={() => sort("naam")}>Naam</th>
@@ -110,7 +156,51 @@ export const PassengerTable: React.FC<{
             </tr>
           ))}
         </tbody>
-      </table>
+      </table> */}
+
+    <Table 
+    aria-label="Passenger-table"
+    sortDescriptor={list.sortDescriptor}
+    onSortChange={list.sort}
+    bottomContent={
+      <div className="flex w-full justify-center">
+        <Pagination
+          isCompact
+          showControls
+          showShadow
+          color="secondary"
+          page={page}
+          total={pages}
+          onChange={(page) => setPage(page)}
+        />
+      </div>
+    }
+    >
+      <TableHeader>
+        <TableColumn key={"firstname"} allowsSorting>Naam</TableColumn>
+        <TableColumn key={"dateofbirth"}>Leeftijd</TableColumn>
+        <TableColumn key={"emergency_contact"}>Noodcontact</TableColumn>
+        <TableColumn key={"emergency_relation"}>Relatie</TableColumn>
+        <TableColumn key={"extra"}>Extra</TableColumn>
+        <TableColumn key={""}> </TableColumn>
+      </TableHeader>
+      
+      <TableBody emptyContent={"Geen passagiers gevonden."} items={items || []} isLoading={isLoading}  loadingContent={<Loader/>}>
+        {(passenger) => (
+          <TableRow key={passenger.id}>
+            {/* {(columnKey) => <TableCell>{getKeyValue(passenger, columnKey)}</TableCell>} */}
+            <TableCell>{passenger.firstname} {passenger.lastname}</TableCell>
+            <TableCell>{calculateAge(passenger.dateofbirth || '')}</TableCell>
+            <TableCell>{`${passenger.emergency_contact || '-'}`}</TableCell>
+            <TableCell>{passenger.emergency_relation || '-'}</TableCell>
+            <TableCell>{passenger.extra || '-'}</TableCell>
+            <TableCell onClick={() => handleEdit(passenger.id)}>...</TableCell>
+          </TableRow>
+          
+        )}
+      </TableBody>
+    </Table>
+
       {isEditPassengerOpen && selectedPassengerId && (
         <EditPassenger id={selectedPassengerId} onClose={closeEditPassenger} />
       )}
