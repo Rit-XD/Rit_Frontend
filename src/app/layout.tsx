4b2de8eeb92f8e2ed128f8356c949b518fd0c205
Bram-Colleman <r0820751@student.thomasmore.be>
2024-05-25 13:17:25 +0200

    add PWA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,16 +3,34 @@ import '@/styles/globals.scss'
 import {GeistSans} from 'geist/font/sans'
 import './globals.css'
 import { NextUIProvider } from '@nextui-org/react'
+import { Metadata } from 'next'
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : 'http://localhost:3000'
 
-export const metadata = {
+export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: 'Rit | Dashboard',
-  description: 'Revolution in transport'
-}
+  title: "Rit | Dashboard",
+  description: "Revolution in transport",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  keywords: ["nextjs", "nextjs13", "next13", "pwa", "next-pwa"],
+  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
+  authors: [
+    { name: "Bram Colleman" },
+    { name: "Senne Christiaens" },
+    { name: "Tibo Vermeire" },
+    
+  ],
+  viewport:
+    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+  icons: [
+    { rel: "apple-touch-icon", url: "icons/icon-128x128.png" },
+    { rel: "icon", url: "icons/icon-128x128.png" },
+  ],
+};
+
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
